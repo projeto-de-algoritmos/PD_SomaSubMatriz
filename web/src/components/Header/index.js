5be34626753f2f8c1a4fb48889b99151b6aa8ed6
Gabriel Davi <gdavi.gd@gmail.com>
2020-12-01 21:25:05 -0300

    Create calculator page
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./styles.css";
 
 function Header() {
-  return <h1>Sub matrix app </h1>;
+  const history = useHistory();
+
+  return (
+    <div className="header-container">
+      <h2>Calcualdora de sub-matriz</h2>
+      <p>
+        Você informa a sua matriz e seus respectivos valores e te mostramos qual
+        a submatriz que possui a maior soma entre seus valores.
+      </p>
+      <button
+        onClick={() => {
+          history.push("/matrix");
+        }}
+      >
+        Começar
+      </button>
+    </div>
+  );
 }
 
 export default Header;
--- web/src/pages/Matrix/index.js
+++ web/src/pages/Matrix/index.js
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
 function Matrix() {
-  return <h1>Matrix</h1>;
+  const [matrixDimension, setMatrixDimension] = useState(0);
+  const [matrixValues, setMatrixValues] = useState([]);
+
+    useEffect(()=>{
+      initMatrix();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[matrixDimension])
+
+  const initMatrix = () => {
+    let newMatrix = [];
+    for (let i = 0; i < matrixDimension; i++) {
+      let newColumns = [];
+      for (let j = 0; j < matrixDimension; j++) {
+        newColumns.push(0);
+      }
+      newMatrix.push(newColumns);
+    }
+    setMatrixValues(newMatrix);
+  };
+
+  const renderMatrix = () => {
+    let matrix = [];
+    if (matrixDimension > 0) {
+      for (let i = 0; i < matrixDimension; i++) {
+        let columns = [];
+        for (let j = 0; j < matrixDimension; j++) {
+          columns.push(
+            <td id={`${i},${j}`} className="table-cell">
+              <input
+                defaultValue={matrixValues[i] && matrixValues[i][j]}
+                type="number"
+                onChange={(event)=>{
+                  let newMatrix = [...matrixValues]; 
+                  newMatrix[i][j] = Number(event.target.value); 
+                  setMatrixValues(newMatrix)
+                }}
+              />
+            </td>
+          );
+        }
+        matrix.push(<tr>{columns}</tr>);
+      }
+
+      return (
+        <div className="matrix">
+          <table>
+            <tbody>{matrix}</tbody>
+          </table>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="matrix-container">
+      <h2>Calculadora</h2>
+      <p>
+        Informe o tamanho de sua matriz no campo abaixo, preencha cada célula e
+        pressione em calcular.
+      </p>
+      <div className="matrix-content">
+        {renderMatrix()}
+        <div className="matrix-input">
+          <label htmlFor="dimension">Qual a dimensão de sua matriz?</label>
+          <input
+            value={matrixDimension}
+            type="number"
+            id="dimension"
+            onChange={(event) => {
+              if (event.target.value <= 20)
+                setMatrixDimension(event.target.value);
+            }}
+          />
+
+          <button>Calcular</button>
+        </div>
+
+        <div className="sum-result">
+          <h3>Soma da total da submatriz</h3>
+          <p>0</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Matrix;
